f5bc87c57f4f62cb10f04ec0ee2f099079d02334
filippo199600 <pippotartaglia@gmail.com>
2023-10-25 16:14:11 +0200

    add relation between models

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -10,4 +10,9 @@ class Project extends Model
     use HasFactory;
 
     protected $fillable = ["title", "url", "content"];
+
+    public function types()
+    {
+        return $this->belongsTo(Type::class);
+    }
 }
